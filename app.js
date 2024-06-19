8ffb74d284ddb0a6b0f3e72e99fc023e7d6efa98
vinay <vinay@idreameducation.org>
2024-06-20 01:12:57 +0530

    installed winston logger and added /user/:name api

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 import express from 'express';
+import winston from 'winston';
 
 const app = express();
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [new winston.transports.Console()],
+});
 
 app.use(express.urlencoded({ limit: '100mb', extended: true }));
 app.use(express.json({ limit: '20mb', extended: true }));
 
 app.get('/', (req, res) => {
-  res.status(200).json({ status: 200, message: 'successful', data: { name: 'Vinay Maurya' } });
+  res.status(200).json({ status: 200, message: 'successful', data: { name: 'This is home page' } });
 });
 
-app.get('/home', (req, res) => {
-  res.status(200).json({ status: 200, message: 'successful', data: 'This is home page' });
+app.get(`/user/:name`, (req, res) => {
+  const userName = req.params.name;
+  res.status(200).json({ status: 200, message: 'successful', data: `Hello ${userName} how are you?` });
 });
 
 app.all('*', (req, res) => {
